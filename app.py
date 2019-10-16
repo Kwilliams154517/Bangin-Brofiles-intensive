beeb1f798f0f2ae289f0747a71af0ef5dd100772
Kyel Williams <Kyewilliams@Kyels-MacBook-Air.local>
2019-10-16 16:36:54 -0700

    CRUD completed only making her look good bro

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient 
+from bson.objectid import ObjectId
 
 client = MongoClient()
 db = client.brofiles
@@ -7,27 +8,60 @@ brofiles = db.broifiles
 
 app = Flask(__name__)
 
-Brofiles =[
-    { 'title': 'Brontent', 'description': 'Search' },
-    { 'title': 'A new brofile', 'description': 'user created'}
-]
+# Brofiles =[
+#     { 'title': 'Brontent', 'description': 'Search' },
+#     { 'title': 'A new brofile', 'description': 'user created'}
+# ]
 
 @app.route('/')
 def brofile_index():
     ''' Brofile Homepage '''
-    return render_template('base.html', brofiles=brofiles.find())
+    return render_template('base.html')
 
 @app.route('/home', methods=['GET', 'POST'])
 def brofile_home():
     """ Brofiles creation page """
-    return render_template('home.html')  
+    return render_template('home.html', brofiles=brofiles.find())  
 
-@app.route('/user/<brofile_id>')
-def show_user_brofile(brofile_id):
-    ''' Shows Brofile page'''
-    # show_brofile()
-    return render_template('show_brofile.html', brofile_id=brofile_id)
+# @app.route('/user/<brofile_id>')
+# def show_user_brofile(brofile_id):
+#     ''' Shows Brofile page'''
+#     # show_brofile()
+#     return render_template('show_brofile.html', brofile_id=brofile_id)
 
+@app.route('/brofiles/new')
+def new_brofiles():
+    """Create a new brofile."""
+    return render_template('new_brofile.html')
+
+@app.route('/brofiles', methods=['POST'])
+def brofiles_submit():
+    """Submit a new playlist."""
+    brofile = {
+        'name': request.form.get('name'),
+        'bio': request.form.get('bio'),
+        'bropiclink': request.form.get('bropiclink'),
+    }
+    brofile_id = brofiles.insert_one(brofile).inserted_id
+    return redirect(url_for('brofiles_show', brofile_id=brofile_id))
+
+@app.route('/brofiles/<brofile_id>')
+def brofiles_show(brofile_id):
+    brofile = brofiles.find_one({'_id': ObjectId(brofile_id)})
+    return render_template('brofiles_show.html', brofile=brofile)
+
+@app.route('/brofiles/<brofile_id>/edit')
+def brofiles_edit(brofile_id):
+    """ edits brofile profile """
+    brofile = brofiles.find_one({'_id': ObjectId(brofile_id)})
+    return render_template('brofiles_edit.html', brofile=brofile)
+
+@app.route('/brofiles/<brofile_id>/delete')
+def brofiles_delete(brofile_id):
+    """ edits brofile profile """
+    brofiles.delete_one({'_id': ObjectId(brofile_id)})
+    return redirect(url_for('brofile_home'))
+    
 
 
 if __name__ == '__main__':
